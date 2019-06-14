847cbc815e1831737d75362bb76995997a259dc2
Aliciea D Suechin <adr330@nyu.edu>
2019-06-14 17:48:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
-def reverse_each_word("Hello there, and how are you?")
-  array.join("Hello there, and how are you?")
+def reverse_each_word(arry)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end 
